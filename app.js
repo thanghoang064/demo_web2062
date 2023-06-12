ea980d379211fb9f15e2c8f890a49ad7dca0f59c
ITPLUS Academy <thanghoang064@gmail.com>
2023-06-12 09:09:51 +0700

    update project

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const productList = document.getElementById('product-list');
 const productForm = document.getElementById('product-form');
 const productName = document.getElementById('product-name');
 const productPrice = document.getElementById('product-price');
+const productId = document.getElementById('product-id');
 
 // tạo ra 1 mảng products để chứa danh sách product hứng từ 
 // json server
@@ -35,8 +36,8 @@ function displayProduct() {
                         <td>${product.name}</td>
                         <td>${product.price}</td>
                         <td>
-                            <button class="edit-btn" >Edit</button>
-                            <button class="delete-btn" >Delete</button>
+                            <button class="edit-btn" data-id="${product.id}" >Edit</button>
+                            <button class="delete-btn" data-id="${product.id}">Delete</button>
                         </td>
                     </tr>
                 `).join('')}
@@ -65,9 +66,77 @@ function addProduct(event) {
     .catch(error => console.error(error));
 }
 // lắng nghe sự kiện submit form để thêm sản phẩm 
-
+// xây dựng hàm hiển thị thông tin trước khi chỉnh sửa 
+function editProduct(event) {
+    // lấy id khi người dùng ấn vào nút edit
+    const productId1 = event.target.dataset.id;
+    //lấy ra đối tượng product theo id 
+    const product = products.find(product => product.id == productId1);
+    // gán vào value 
+    productName.value = product.name;
+    productPrice.value = product.price;
+    productId.value = product.id;
+}
+function updateProduct(event) {
+    event.preventDefault();
+    // khai báo 1 đối tượng product mới khi đã chỉnh sửa
+    const product = {
+        name : productName.value,
+        price : productPrice.value
+    }
+    const productId = document.getElementById('product-id').value;
+    fetch(`http://localhost:3000/products/${productId}`,{
+        method: 'PUT', // phương thức push tương đương với sửa 
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(product) // ép về chuỗi JSON để nhồi dữ liệu mới vào product
+    }).then(response=> response.json())
+    .then(data=>{
+        const index = products.findIndex(product => product.id == productId);
+        products[index] = data;
+        displayProduct(); // gọi lại hàm hiển thị để cập nhập lại bảng 
+        // reset lại form
+        productForm.reset();
+    })
+    .catch(error => console.error(error));
+}
 productForm.addEventListener('submit',event=> {
-    addProduct(event);
+    //check nếu tồn tại productID thì gọi hàm update ngược lại gọi hàm add 
+    if(productId.value) {
+        updateProduct(event);
+    } else {
+        addProduct(event);
+    }
+});
+function deleteProduct(event) {
+    const productID = event.target.dataset.id;
+    fetch(`http://localhost:3000/products/${productID}`,{
+        method: 'DELETE',
+    }).then(response=> {
+         // xóa khỏi danh sách ở bộ nhớ tạm
+         products = products.filter(product=>product.id != productID);
+         displayProduct(); // gọi lại hàm hiển thị để cập nhập lại bảng 
+    })
+   
+    .catch(error => console.error(error));
+}
+// lắng nghe sự kiện khi click nút edit để bắt dữ liệu lên ô input
+productList.addEventListener('click',event=>{
+
+    // khi người dùng ấn vào nút edit-btn
+    if(event.target.classList.contains('edit-btn')) {
+         editProduct(event);
+        // console.log(123);
+    }
+});
+productList.addEventListener('click',event=>{
+
+    // khi người dùng ấn vào nút edit-btn
+    if(event.target.classList.contains('delete-btn')) {
+         deleteProduct(event);
+        // console.log(123);
+    }
 });
 
 getProduct();
